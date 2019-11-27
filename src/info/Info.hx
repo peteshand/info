a302a30649b575d90a8f2803e5ae47c711ed7587
P.J.Shand <pete@peteshand.net>
2019-11-27 11:13:24 +1100

    Add Info static class

--- src/info/Info.hx
+++ src/info/Info.hx
@@ -1,5 +1,6 @@
 package info;
 
+import info.app.App;
 import info.device.DeviceInfo;
 import info.browser.Browser;
 import info.browser.BrowserInfo;
@@ -7,54 +8,75 @@ import info.os.OSInfo;
 import info.os.OS;
 import info.screen.ScreenInfo;
 import info.screen.Orientation;
-import geom.Size;
 
 /**
  * ...
  * @author P.J.Shand
  */
-class Info
-{
+class Info {
+	public static var appId(get, null):String;
+	public static var appFilename(get, null):String;
+	public static var appVersion(get, null):String;
+	public static var appDir(get, null):String;
 	public static var browser(get, null):Browser;
-	inline static function get_browser():Browser
-	{
+	public static var isMobile(get, null):Null<Bool>;
+	public static var os(get, null):OS;
+	public static var highDPI(get, null):Bool;
+	public static var dpi(get, null):Null<Float>;
+	public static var size(get, null):{width:Int, height:Int};
+	public static var orientation(get, null):Orientation;
+	public static var width(get, null):Int;
+	public static var height(get, null):Int;
+
+	inline static function get_appId():String {
+		return App.appId;
+	}
+
+	inline static function get_appFilename():String {
+		return App.appFilename;
+	}
+
+	inline static function get_appVersion():String {
+		return App.appVersion;
+	}
+
+	inline static function get_appDir():String {
+		return App.appDir;
+	}
+
+	inline static function get_browser():Browser {
 		return BrowserInfo.browser;
 	}
 
-	public static var isMobile(get, null):Null<Bool>;
-	static function get_isMobile():Null<Bool>
-	{
+	static function get_isMobile():Null<Bool> {
 		return DeviceInfo.isMobile;
 	}
 
-	public static var os(get, null):OS;
-	static function get_os():OS
-	{
+	static function get_os():OS {
 		return OSInfo.os;
 	}
 
-	public static var highDPI(get, null):Bool;
-	static function get_highDPI():Bool
-	{
+	static function get_highDPI():Bool {
 		return ScreenInfo.highDPI;
 	}
 
-	public static var dpi(get, null):Null<Float>;
-	static function get_dpi():Null<Float>
-	{
+	static function get_dpi():Null<Float> {
 		return ScreenInfo.dpi;
 	}
 
-	public static var size(get, null):Size;
-	static function get_size():Size
-	{
+	static function get_size():{width:Int, height:Int} {
 		return ScreenInfo.size;
 	}
 
-	public static var orientation(get, null):Orientation;
-	static function get_orientation():Orientation
-	{
+	static function get_orientation():Orientation {
 		return ScreenInfo.orientation;
 	}
 
+	inline static function get_width():Int {
+		return App.width;
+	}
+
+	inline static function get_height():Int {
+		return App.height;
+	}
 }
--- src/info/screen/ScreenInfo.hx
+++ src/info/screen/ScreenInfo.hx
@@ -1,79 +1,77 @@
 package info.screen;
 
-import geom.Size;
 #if lime
-	import lime.system.System;
+import lime.system.System;
 #end
 #if html5
-	import js.Browser;
-	import js.html.DivElement;
+import js.Browser;
+import js.html.DivElement;
 #end
 
 /**
  * ...
  * @author P.J.Shand
  */
-class ScreenInfo
-{
+class ScreenInfo {
 	public static var highDPI(get, null):Bool;
 	public static var dpi(get, null):Null<Float>;
-	public static var size(get, null):Size;
+	public static var size(get, null):{width:Int, height:Int};
 	public static var orientation(get, null):Orientation;
 	public static var _dpi:Null<Float>;
 
-	static function get_highDPI():Bool
-	{
+	static function get_highDPI():Bool {
 		#if html5
-			if (Browser.window.devicePixelRatio >= 2) return true;
-			else return false;
+		if (Browser.window.devicePixelRatio >= 2)
+			return true;
+		else
+			return false;
 		#end
 		throw "needs to be implemented";
 		return false;
 	}
 
-	static function get_dpi():Float
-	{
+	static function get_dpi():Float {
 		#if lime
-			return System.getDisplay(0).dpi;
+		return System.getDisplay(0).dpi;
 		#elseif html5
-			if (_dpi == null){
-				var dpiDiv = Browser.window.document.createDivElement();
-				dpiDiv.id = 'dpi';
-				dpiDiv.style.height = '1in';
-				dpiDiv.style.left = '-100%';
-				dpiDiv.style.position = 'absolute';
-				dpiDiv.style.top = '-100%';
-				dpiDiv.style.width = '1in';
+		if (_dpi == null) {
+			var dpiDiv = Browser.window.document.createDivElement();
+			dpiDiv.id = 'dpi';
+			dpiDiv.style.height = '1in';
+			dpiDiv.style.left = '-100%';
+			dpiDiv.style.position = 'absolute';
+			dpiDiv.style.top = '-100%';
+			dpiDiv.style.width = '1in';
 
-				Browser.window.document.body.appendChild(dpiDiv);
-				_dpi = dpiDiv.offsetHeight * Browser.window.devicePixelRatio;
-				Browser.window.document.body.removeChild(dpiDiv);
-			}
-			return _dpi;
+			Browser.window.document.body.appendChild(dpiDiv);
+			_dpi = dpiDiv.offsetHeight * Browser.window.devicePixelRatio;
+			Browser.window.document.body.removeChild(dpiDiv);
+		}
+		return _dpi;
 		#end
 		throw "needs to be implemented";
 	}
 
-	static function get_size():Size
-	{
+	static function get_size():{width:Int, height:Int} {
 		#if html5
-			return { 
-				width: Browser.window.screen.width, 
-				height: Browser.window.screen.height
-			}
+		return {
+			width: Browser.window.screen.width,
+			height: Browser.window.screen.height
+		}
 		#end
 		throw "needs to be implemented";
 		return null;
 	}
 
-	static function get_orientation():Orientation
-	{
-		if (size == null){
+	static function get_orientation():Orientation {
+		if (size == null) {
 			throw "needs to be implemented";
 			return null;
 		} else {
-			if (size.width > size.height) return Orientation.LANDSCAPE;
-			else return Orientation.PORTRAIT;
+			if (size.width > size.height)
+				return Orientation.LANDSCAPE;
+			else
+				return Orientation.PORTRAIT;
 		}
 	}
 }
